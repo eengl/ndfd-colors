4f6ebb72cb3e52a5cfd91bd3a4d46e4e470aa619
Eric Engle <eengl@users.noreply.github.com>
2017-10-03 15:17:55 -0400

    Update colortable-to-cpt.py
    Modified so it will write to file instead of print to screen.

--- scripts/colortable-to-cpt.py
+++ scripts/colortable-to-cpt.py
@@ -1,17 +1,40 @@
 #!/usr/bin/env python
 
+# ---------------------------------------------------------------------------------------- 
+# Import modules
+# ---------------------------------------------------------------------------------------- 
 import re
 import sys
 
-colortableFile = sys.argv[1]
+# ---------------------------------------------------------------------------------------- 
+# Test for command arguments
+# ---------------------------------------------------------------------------------------- 
+if len(sys.argv) != 3:
+    print "usage: ./",sys.argv[0]," <COLORTABLE_FILE> <CPT_FILE>"
+    exit(1)
 
+# ---------------------------------------------------------------------------------------- 
+# Get command arguments
+# ---------------------------------------------------------------------------------------- 
+colortableFile = sys.argv[1]
+cptFile = sys.argv[2]
+
+# ---------------------------------------------------------------------------------------- 
+# Initialize lists
+# ---------------------------------------------------------------------------------------- 
 dVal = []
 rVal = []
 gVal = []
 bVal = []
 
-with open("colortable/"+colortableFile+".colortable",'r') as f: lines = f.read().splitlines()
+# ---------------------------------------------------------------------------------------- 
+# Read .colortable file
+# ---------------------------------------------------------------------------------------- 
+with open(colortableFile) as f: lines = f.read().splitlines()
 
+# ---------------------------------------------------------------------------------------- 
+# Parse colortable data
+# ---------------------------------------------------------------------------------------- 
 for i,line in enumerate(lines):
     if i > 8:
         ctdata = re.sub(' +',' ',line.replace("\t"," ")).split(" ")
@@ -20,12 +43,16 @@ for i,line in enumerate(lines):
         gVal.append(int(ctdata[2]))
         bVal.append(int(ctdata[3]))
 
-ncolors = len(dVal)
-
-dVal = [x * 25.4 for x in dVal]
-
+# ---------------------------------------------------------------------------------------- 
 # Print .cpt file to screen
-print "# NDFD-"+colortableFile.replace(".colortable","")+".cpt"
-print "# COLOR_MODEL = RGB"
+# ---------------------------------------------------------------------------------------- 
+fout = open(cptFile,"w")
+fout.write("# NDFD-"+colortableFile.replace(".colortable","")+".cpt\n")
+fout.write("# COLOR_MODEL = RGB\n")
 for i in range(len(dVal)-1):
-    print "%f %3d %3d %3d %f %3d %3d %3d" % (dVal[i],rVal[i],gVal[i],bVal[i],dVal[i+1],rVal[i+1],gVal[i+1],bVal[i+1])
+    fout.write("%f %3d %3d %3d %f %3d %3d %3d\n" % (dVal[i],rVal[i],gVal[i],bVal[i],dVal[i+1],rVal[i+1],gVal[i+1],bVal[i+1]))
+
+# ---------------------------------------------------------------------------------------- 
+# Close output file
+# ---------------------------------------------------------------------------------------- 
+fout.close()
